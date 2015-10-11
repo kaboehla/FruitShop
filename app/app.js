400b7e9567c774150a761bdad43b228762d47f30
kaboehla <boehland@gmx.de>
2015-10-11 13:41:02 +0200

    layout überarbeitet

--- app/app.js
+++ app/app.js
@@ -29,13 +29,13 @@ app.run(function ($rootScope, $location, $route) {
 app.controller("FruitController", function FruitController($scope) {
 
     $scope.fruits = [
-        {id: 1, name: 'Boskop', family : 'Apfel'},
-        {id: 2, name: 'Elmar', family : 'Apfel'},
-        {id: 3, name: 'Sauerkirsche', family : 'Kirsche'},
-        {id: 4, name: 'Amarena-Kirsche', family : 'Kirsche'},
-        {id: 5, name: 'Achener Hausapfel', family : 'Apfel'},
-        {id: 6, name: 'Alantapfel', family : 'Apfel'},
-        {id: 7, name: 'Pfaffenapfel', family : 'Apfel'}
+        {id: 1, name: 'Boskop', familiy : 'Apfel', preis : 3.4},
+        {id: 2, name: 'Elmar', family : 'Apfel', preis : 2.5},
+        {id: 3, name: 'Sauerkirsche', family : 'Kirsche', preis : 1.8},
+        {id: 4, name: 'Amarena-Kirsche', family : 'Kirsche', preis : 3.2},
+        {id: 5, name: 'Achener Hausapfel', family : 'Apfel', preis : 2.2},
+        {id: 6, name: 'Alantapfel', family : 'Apfel', preis : 1.2},
+        {id: 7, name: 'Pfaffenapfel', family : 'Apfel', preis : 0.3}
     ];
 
     $scope.gemuese = [
